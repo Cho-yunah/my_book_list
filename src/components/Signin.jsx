c7fbf866f0cefd88be99a9d18fb48a9d10a3bcbf
YUNA <favoryun12@gmail.com>
2020-12-23 12:58:00 +0900

    input 상태 변경가능  컴포넌트로 만듬

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,63 +1,88 @@
+import React from 'react';
 import { Row, Col, Input, Button } from 'antd';
 import styles from './Signin.module.css';
 
-const Signin = () => {
-  return (
-    <Row align="middle" className={styles.signin_row}>
-      <Col span={24}>
-        <Row className={styles.signin_contents}>
-          <Col span={12}>
-            <img
-              src="/img/bg_signin.png"
-              alt="Signin"
-              className={styles.signin_bg}
-            />
-          </Col>
-          <Col span={12}>
-            <div className={styles.signin_title}> My Books</div>
-            <div className={styles.signin_subtitle}>
-              Please Note YOur Opinion
-            </div>
-            <div className={styles.signin_underline} />
-            <div className={styles.email_title}>
-              Email
-              <span className={styles.required}> *</span>
-            </div>
-            <div className={styles.input_area}>
-              <Input
-                name="email"
-                placeholder="email"
-                className={styles.Input}
-              />
-            </div>
-            <div className={styles.email_title}>
-              Password
-              <span className={styles.required}> *</span>
-            </div>
-            <div className={styles.input_area}>
-              <Input
-                type="password"
-                autoComplete="current-password"
-                className={styles.Input}
-              />
-            </div>
-            <div className={styles.button_area}>
-              <Button
-                size="large"
-                loading={false}
-                className={styles.button}
-                onClick={click}
-              >
-                Sign In
-              </Button>
-            </div>
+class Signin extends React.Component {
+  state = {
+    email: '기본값',
+  };
+  render() {
+    // console.log(this.state.email);
+    const regexEmail = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+    const isEmail = regexEmail.test(this.state.email);
+
+    return (
+      <form>
+        <Row align="middle" className={styles.signin_row}>
+          <Col span={24}>
+            <Row className={styles.signin_contents}>
+              <Col span={12}>
+                <img
+                  src="/img/bg_signin.png"
+                  alt="Signin"
+                  className={styles.signin_bg}
+                />
+              </Col>
+              <Col span={12}>
+                <div className={styles.signin_title}> My Books</div>
+                <div className={styles.signin_subtitle}>
+                  Please Note YOur Opinion
+                </div>
+                <div className={styles.signin_underline} />
+                <div className={styles.email_title}>
+                  Email
+                  <span className={styles.required}> *</span>
+                </div>
+                <div className={styles.input_area}>
+                  {/* <Input
+                    name="email"
+                    placeholder="email"
+                    className={styles.Input}
+                  /> */}
+                  <input
+                    type="text"
+                    value={this.state.email}
+                    onChange={this.change}
+                  />
+                  {isEmail ? '이메일 맞음' : '이메일 아님'}
+                </div>
+                <div className={styles.email_title}>
+                  Password
+                  <span className={styles.required}> *</span>
+                </div>
+                <div className={styles.input_area}>
+                  <Input
+                    type="password"
+                    autoComplete="current-password"
+                    className={styles.Input}
+                  />
+                </div>
+                <div className={styles.button_area}>
+                  <Button
+                    size="large"
+                    loading={false}
+                    className={styles.button}
+                    onClick={this.click}
+                  >
+                    Sign In
+                  </Button>
+                </div>
+              </Col>
+            </Row>
           </Col>
         </Row>
-      </Col>
-    </Row>
-  );
+      </form>
+    );
+  }
 
-  function click() {}
-};
+  click = () => {
+    // console.log('clicked', this.state.email);
+  };
+
+  change = (e) => {
+    // console.log('change', e.target.value);
+    this.setState({ email: e.target.value });
+  };
+}
 
 export default Signin;
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,5 +1,7 @@
 import Signin from '../components/Signin';
 
 export default function SigninPage() {
+  // path => 데이터로 바꿔서 무언가 처리할때
+  // auth
   return <Signin />;
 }
